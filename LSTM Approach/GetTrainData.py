bfe8642a6805bcd9938d5f3a5aeb804fd640cc35
Guwalgiya <guanight@gmail.com>
2018-03-20 15:20:55 -0400

    - Experiment

--- LSTM Approach/GetTrainData.py
+++ LSTM Approach/GetTrainData.py
@@ -13,10 +13,10 @@ def main(training_path, block_length):
 def block_sequence(seq_interval, seq_finger, block_length):
     seq_input = []
     seq_label = []
-    for i in range(len(seq_interval) - (block_length-1)):
+    for i in range(len(seq_interval) - (block_length - 1)):
         seq_label.append(seq_finger[i + block_length])
         temp_block = []
-        for j in xrange(block_length):
+        for j in range(block_length):
             temp_block.append(seq_finger[i+j])
             temp_block.append(seq_interval[i+j])
         seq_input.append(temp_block)
--- LSTM Approach/StoreTrainData.py
+++ LSTM Approach/StoreTrainData.py
@@ -1,7 +1,8 @@
 import GetTrainData
 import pickle
-block_length = 4
-training_path = '../Datasets/Czerny_599_with_Fingering'
+block_length           = 4
+training_path          = '../Datasets/Complete_Dataset'
 input_list, label_list = GetTrainData.main(training_path, block_length)
+label_list             = [int(x * 5) for x in label_list]
 pickle.dump(input_list, open('../Datasets/processed/input_list.pkl', 'wb'))
 pickle.dump(label_list, open('../Datasets/processed/label_list.pkl', 'wb'))
