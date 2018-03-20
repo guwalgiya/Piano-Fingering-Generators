369f8a49d7cc0dfb947071915a99c08076fece7d
GuitarmonYz <guitarmonyz@gmail.com>
2018-03-20 15:12:04 -0400

    change test method

--- LSTM Approach/StoreTrainData.py
+++ LSTM Approach/StoreTrainData.py
@@ -1,7 +1,7 @@
 import GetTrainData
 import pickle
 block_length = 4
-training_path = '../Datasets/Czerny_599_with_Fingering'
+training_path = '../Datasets/Complete_Dataset'
 input_list, label_list = GetTrainData.main(training_path, block_length)
 pickle.dump(input_list, open('../Datasets/processed/input_list.pkl', 'wb'))
 pickle.dump(label_list, open('../Datasets/processed/label_list.pkl', 'wb'))
