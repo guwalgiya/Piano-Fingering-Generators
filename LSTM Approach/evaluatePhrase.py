8b344096fe0cee015350e97d99b9bce28ed95afe
Guwalgiya <guanight@gmail.com>
2018-03-21 15:43:37 -0400

    - update

--- LSTM Approach/evaluatePhrase.py
+++ LSTM Approach/evaluatePhrase.py
@@ -1,19 +1,20 @@
-import intervalBook.wrong_combo as wrong_combo
-import intervalBook.valid_combo as valid_combo
-
+from IntervalBook import wrong_combo, valid_combo
 def main(interval_list, predicted_fingers, Gt_fingers):
     
     num_abs_true                     =  0
     num_abs_false                    =  0
     num_not_good                     =  0
     # predicted_fingers & Gt_fingers have one more element than interval_list does
-    for i in range(len(interval_list)-1):
+    for i in range(len(interval_list) - 1):
         if  predicted_fingers[i + 1] == Gt_fingers[i + 1]:
             num_abs_true             += 1
-        else:
-            num_abs_false            += sanityCheck (interval_list[i],  (predicted_fingers[i], predicted_fingers[i + 1]))
-            num_not_good             += qualityCheck(interval_list[i], (predicted_fingers[i], predicted_fingers[i + 1]))
-    return num_abs_true, num_abs_false
+            num_abs_false            += sanityCheck (interval_list[i],  [predicted_fingers[i], predicted_fingers[i + 1]])
+            num_not_good             += qualityCheck(interval_list[i],  [predicted_fingers[i], predicted_fingers[i + 1]])
+        else:            
+            num_abs_false            += sanityCheck (interval_list[i],  [predicted_fingers[i], predicted_fingers[i + 1]])
+            num_not_good             += qualityCheck(interval_list[i],  [predicted_fingers[i], predicted_fingers[i + 1]])
+
+    return num_abs_true, num_abs_false, num_not_good
 
 def sanityCheck(current_interval, current_finger_combo):
     if current_interval > 0:
