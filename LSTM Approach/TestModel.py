e6896236fdd0bee4538a3dec6154241555ef886b
GuitarmonYz <guitarmonyz@gmail.com>
2018-03-21 22:06:40 -0400

    refine code in TestModel

--- LSTM Approach/TestModel.py
+++ LSTM Approach/TestModel.py
@@ -37,24 +37,35 @@ with tf.Session() as session:
         while test_step < generate_step - 3:
             np_init_state = np.reshape(np.array(init_state), [-1, N_INPUT, 1])
             onehot_pred_test = session.run(pred, feed_dict={x: np_init_state, keep_prob: 1})
-            # print(onehot_pred_test)
             _, top_2 = tf.nn.top_k(onehot_pred_test[0], 2)
             finger_pred_first = int(top_2[0].eval())+1
             finger_pred_second = int(top_2[1].eval())+1
-            finger_pred = int(tf.argmax(onehot_pred_test, 1).eval())+1
+            # finger_pred = int(tf.argmax(onehot_pred_test, 1).eval())+1
+            finger_combo = [init_state[-2], finger_pred_first]
+            if evaluatePhrase.qualityCheck(init_state[-1], finger_combo):
+                finger_combo = [init_state[-2], finger_pred_second]
+                if evaluatePhrase.qualityCheck(init_state[-1], finger_combo):
+                    finger_pred = finger_pred_first
+                else:
+                    finger_pred = finger_pred_second
+            else:
+                finger_pred = finger_pred_first
+            
             print(str(init_state) + "->" + str(finger_pred))
             temp_finger_res += [finger_pred]
             if test_step < generate_step - 4:
                 init_state = generateNewState(init_state, finger_pred, test_interval[test_step+4], False)
             test_step+=1
         temp_finger_res = [test_finger[3]] + temp_finger_res
-        print(len(temp_finger_res))
+        print('number of notes: '+len(temp_finger_res))
         absTrue, absFalse, notGood = evaluatePhrase.main(test_interval[3:], temp_finger_res, test_finger[3:])
         total_absTrue += absTrue
         total_absFalse += absFalse
         total_notGood += notGood
         total_interval_len += (generate_step - 3)
-        print(absTrue, absFalse, notGood)
+        print('absolute acc: ' + absTrue, 'absolute wrong: ' + absFalse, 'not ideal: ' + notGood)
         print("Elapsed time: ", elapsed(time.time() - start_time))
         print("Testing finished")
-    print(total_absTrue/float(total_interval_len+len(input_list)), total_absFalse/float(total_interval_len), total_notGood/float(total_interval_len)) 
+    print('absolute acc: ' + total_absTrue/float(total_interval_len+len(input_list)), \
+          'absolute false: ' + total_absFalse/float(total_interval_len), \
+          'not ideal: ' + total_notGood/float(total_interval_len)) 
