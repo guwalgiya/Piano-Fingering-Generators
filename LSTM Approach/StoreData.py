21073eeb4e2c69f6a3eadca301c1037cf0d22ca0
GuitarmonYz <guitarmonyz@gmail.com>
2018-03-22 22:01:19 -0400

    further class name refactoring

--- LSTM Approach/StoreData.py
+++ LSTM Approach/StoreData.py
@@ -1,12 +1,12 @@
-import GetTrainData
+import GetBlockedData
 import pickle
 from parameters import BLOCK_LENGTH
 normalization          = False
 training_path          = '../Datasets/Complete_Training_Dataset'
 testing_path           = '../Datasets/Complete_Testing_Dataset'
 
-train_input_list, train_label_list = GetTrainData.main(training_path, BLOCK_LENGTH, True, normalization)
-test_input_list, test_label_list = GetTrainData.main(testing_path, BLOCK_LENGTH, False, normalization)
+train_input_list, train_label_list = GetBlockedData.main(training_path, BLOCK_LENGTH, True, normalization)
+test_input_list, test_label_list = GetBlockedData.main(testing_path, BLOCK_LENGTH, False, normalization)
 if normalization:
     train_label_list             = [int(x * 5) for x in train_label_list]
     test_label_list              = [int(x * 5) for x in test_label_list]
