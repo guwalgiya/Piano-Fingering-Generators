eef3eb8d66463286c6c20dc3732486e7fa9c53a3
guitarmonyz <guitarmonyz@gmail.com>
2020-05-14 10:30:43 -0400

    revert 01 in interval book

--- LSTM Approach/EvaluateJPMethod.py
+++ LSTM Approach/EvaluateJPMethod.py
@@ -3,13 +3,15 @@ from JPDataPreProcessing import getListsFromFilenames, getListsFromSingeFile
 from EvaluateVectorPhrase import main
 
 def evaluate_yz(filenames, gt_dir, est_dir):
-    _, gt_finger_list, interval_list, accidental_list = getListsFromFilenames(filenames, gt_dir)
-    _, est_finger_list, _, _ = getListsFromFilenames(filenames, est_dir)
-    bw_list = [[s, e] for s, e in zip(accidental_list[:-1], accidental_list[1:])]
-    num_abs_true, num_abs_false, num_not_ideal = main(interval_list, est_finger_list, bw_list, gt_finger_list)
-    print('absolute true: {}'.format(float(num_abs_true) / len(interval_list)))
-    print('absolute false: {}'.format(float(num_abs_false) / len(interval_list)))
-    print('not ideal: {}'.format(float(num_not_ideal) / len(interval_list)))
+    for filename in filenames:
+        _, gt_finger_list, interval_list, accidental_list, _ = getListsFromSingeFile(filename, gt_dir)
+        _, est_finger_list, _, _, _ = getListsFromSingeFile(filename, est_dir)
+        bw_list = [[s, e] for s, e in zip(accidental_list[:-1], accidental_list[1:])]
+        num_abs_true, num_abs_false, num_not_ideal = main(interval_list, est_finger_list, bw_list, gt_finger_list)
+        print(filename + ': ')
+        print('absolute true: {}'.format(float(num_abs_true) / len(interval_list)))
+        print('absolute false: {}'.format(float(num_abs_false) / len(interval_list)))
+        print('not ideal: {}'.format(float(num_not_ideal) / len(interval_list)))
 
 def evaluate_jp(filenames, gt_dir, res_finger_list, res_id_list):
     gt_finger_lists = []
