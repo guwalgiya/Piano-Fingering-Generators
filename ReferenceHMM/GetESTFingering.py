c76831b746d953a77b19d654cc5b2b3736fb1213
guwalgiya <guanight@gmail.com>
2021-05-14 22:34:53 -0400

    os-join

--- ReferenceHMM/GetESTFingering.py
+++ ReferenceHMM/GetESTFingering.py
@@ -25,19 +25,30 @@ def trainHmm():
     subprocess.call(['./train_hmm.sh'])
 
 def runHmm(filenames, hmm_type, default=False):
+
     for filename in filenames:
         if default:
-            subprocess.call(['./run_'+hmm_type+'.sh', INPUT_DIR+filename, OUTPUT_DIR+'/default/'+hmm_type+'/'+filename])
+            subprocess.call(['./run_'+hmm_type+'.sh', INPUT_DIR+filename, OUTPUT_DIR+'default/'+hmm_type+'/'+filename])
         else:
-            subprocess.call(['./run_trained_'+hmm_type+'.sh', INPUT_DIR+filename, OUTPUT_DIR+'/selfTrained/'+hmm_type+'/'+filename])
-
+            subprocess.call(
+                [
+                    './run_trained_{}.sh'.format(hmm_type), 
+                    os.path.join(INPUT_DIR, filename), 
+                    os.path.join(
+                        OUTPUT_DIR,
+                        'selfTrained',
+                        hmm_type,
+                        filename
+                    )
+                ]
+            )
 def convertToCsv(input_dir, output_dir):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
     for _, _, filenames in os.walk(input_dir):
         for filename in filenames:
             filename_ = filename.split('.')[0]
-            with open(input_dir+filename, 'r') as finger_data_in, open(output_dir+filename_+'.csv', 'w') as finger_data_out:
+            with open(input_dir+'/' + filename, 'r') as finger_data_in, open(output_dir + '/' + filename_ + '.csv', 'w') as finger_data_out:
                 finger_data_in.readline()
                 for line in finger_data_in:
                     line = line.replace('\t', ',')
